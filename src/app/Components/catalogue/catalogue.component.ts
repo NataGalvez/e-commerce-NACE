1df0d5966d80486486582aa0f0ee2457a4f6f4ef
Nata Galvez <natagalvez>
2020-01-15 12:45:31 -0300

    show data

--- src/app/Components/catalogue/catalogue.component.ts
+++ src/app/Components/catalogue/catalogue.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import products from "../../Models/products.json";
 @Component({
-  selector: 'app-catalogue',
-  templateUrl: './catalogue.component.html',
-  styleUrls: ['./catalogue.component.css']
+  selector: "app-catalogue",
+  templateUrl: "./catalogue.component.html",
+  styleUrls: ["./catalogue.component.css"]
 })
 export class CatalogueComponent implements OnInit {
+  listProducts: any[] = products;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
