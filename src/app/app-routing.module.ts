ce6ac09f0f9b0b15257336408797b56997f6b314
Nata Galvez <natagalvez>
2020-01-15 00:12:34 -0300

    routing catalogue ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CatalogueComponent } from "./Components/catalogue/catalogue.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "catalogue",
+    component: CatalogueComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
